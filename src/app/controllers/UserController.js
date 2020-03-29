fcf5b4c1410ad7f35727b78585ddbf661205b7bc
Genesson Andrei Sauer <genesson_sauer@hotmail.com>
2020-03-29 18:50:12 -0300

    feat: create model and controller user

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,33 @@
+import User from '../models/User';
+
+class UserController {
+  async index(req, res) {
+    return res.json();
+  }
+
+  async show(req, res) {
+    return res.json();
+  }
+
+  async store(req, res) {
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExists) {
+      return res.status(400).json({ erro: 'User already exists.' });
+    }
+
+    const { id, name, email, provider } = await User.create(req.body);
+
+    return res.json({ id, name, email, provider });
+  }
+
+  async update(req, res) {
+    return res.json();
+  }
+
+  async delete(req, res) {
+    return res.json();
+  }
+}
+
+export default new UserController();
