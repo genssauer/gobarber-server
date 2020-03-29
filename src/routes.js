fb9ed8c49e1e4b056bb10d6f4df52a4137530419
Genesson Andrei Sauer <genesson_sauer@hotmail.com>
2020-03-29 16:46:01 -0300

    chore: Install ESLint, Prettier & EditorConfig

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/users', (req, res) => {
-  return res.json({ message: 'Hello Gens'});
-});
+routes.get('/users', (req, res) => res.json({ message: 'Hello Gens' }));
 
 export default routes;
