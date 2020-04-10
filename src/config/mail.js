99378ac64e1e7597ad4261847950ae6ee7617edb
Genesson Andrei Sauer <genesson_sauer@hotmail.com>
2020-04-10 18:09:45 -0300

    feat: configure templates of mails

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '4e0a6c52e1ee47',
+    pass: '08b5943dd07bde',
+  },
+  secure: false,
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
